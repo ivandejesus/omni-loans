a076af5cea11460fc0186863331409a3fe632372
Ivan De Jesus <ivan.dejesus@zmtsys.com>
2022-08-01 08:49:44 +0800

    [WIP] add vuejs frontend setup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (Request $request) {
+    return view('omni-loans');
 });
